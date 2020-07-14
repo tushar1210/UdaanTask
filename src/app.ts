fe56b4584cfcd99cecddd0436f07fbb41c053a0a
tushar.1210 <ts26192271@gmail.com>
2020-07-14 19:05:35 +0530

    try fixing cors

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,24 @@ mongoose.Promise = global.Promise ;
 const PORT = process.env.PORT || 50;
 // Instatiate an express App
 var app = express();
-app.set('json spaces', 4);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.all('*', function(req, res, next) {
+    var origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
+
+
+app.set('json spaces', 4);
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 app.use(express.static(__dirname + '/Views'));
 app.get('/',(request,response)=>{
     response.sendFile(__dirname+"/Views/index.html");
